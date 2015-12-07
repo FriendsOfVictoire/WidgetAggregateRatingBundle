b981f0b8fb8474d0d26ae2be6590a5ad6e1bf727
Charlie <charlielucas.cl@gmail.com>
2015-12-07 16:06:41 +0100

    add options for size and message to show in widget form

--- Form/WidgetAggregateRatingType.php
+++ Form/WidgetAggregateRatingType.php
@@ -5,6 +5,7 @@ namespace Victoire\Widget\AggregateRatingBundle\Form;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Victoire\Bundle\CoreBundle\Form\WidgetType;
+use Victoire\Widget\AggregateRatingBundle\Entity\WidgetAggregateRating;
 
 
 /**
@@ -20,10 +21,14 @@ class WidgetAggregateRatingType extends WidgetType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('bestRating', null, array(
-                    'label' => 'widget_aggregaterating.form.bestRating.label'
-            ))            ->add('worstRating', null, array(
-                    'label' => 'widget_aggregaterating.form.worstRating.label'
+            ->add('options_size', "choice", array(
+                    'property_path' => "options[size]",
+                    'choices' => $builder->getData()->getSizesAvailable(),
+                    'label' => 'widget_aggregaterating.form.options.size.label'
+            ))
+            ->add('message', "choice", array(
+                    'choices' => $builder->getData()->getMessagesAvailable(),
+                    'label' => 'widget_aggregaterating.form.message.label'
             ));
         parent::buildForm($builder, $options);
 
