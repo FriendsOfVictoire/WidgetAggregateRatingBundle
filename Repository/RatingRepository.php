2f5f11bfd965e3876a88d7fbba9f8033fd7a39ec
Charlie <charlielucas.cl@gmail.com>
2015-12-14 18:40:24 +0100

    add methods to filter and to get average and number of ratings

--- Repository/RatingRepository.php
+++ Repository/RatingRepository.php
@@ -2,6 +2,7 @@
 namespace Victoire\Widget\AggregateRatingBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Victoire\Widget\AggregateRatingBundle\Entity\Rating;
 
 class RatingRepository extends EntityRepository
@@ -17,7 +18,31 @@ class RatingRepository extends EntityRepository
                 $setter = 'set'.ucfirst($key);
                 $rating->$setter($value);
             }
+            $rating->setValue($this->getRatingValues([
+                'businessEntityId' => $criteria['businessEntityId'],
+                'entityId' => $criteria['entityId']
+            ])['ratingRound']);
         }
         return $rating;
+    }
+
+    public function filterByCriteria(QueryBuilder $qb, array $criteria)
+    {
+        foreach($criteria as $key => $value)
+        {
+            $qb->andWhere('rating.'.$key.' = :'.$key)
+                ->setParameter(':'.$key, $value);
+
+        }
+        return $qb;
+    }
+
+    public function getRatingValues(array $criteria)
+    {
+        $qb = $this->createQueryBuilder('rating')
+            ->select('avg(rating.value) as ratingRound, count(rating) as ratingNumber');
+        $qb = $this->filterByCriteria($qb, $criteria);
+
+        return $qb->getQuery()->getResult()[0];
     }
 }
