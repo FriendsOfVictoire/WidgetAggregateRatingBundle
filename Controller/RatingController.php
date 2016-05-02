cff4af3932444588574a1eecbfb665435e1a71f5
Charlie <charlielucas.cl@gmail.com>
2016-05-02 16:14:59 +0200

    regenerate cache for pages associated when a rating is add/update

--- Controller/RatingController.php
+++ Controller/RatingController.php
@@ -8,7 +8,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Victoire\Bundle\BusinessPageBundle\Entity\BusinessPage;
+use Victoire\Bundle\BusinessPageBundle\Entity\BusinessTemplate;
+use Victoire\Bundle\CoreBundle\Entity\View;
+use Victoire\Bundle\PageBundle\Entity\Page;
+use Victoire\Bundle\WidgetBundle\Entity\Widget;
 use Victoire\Widget\AggregateRatingBundle\Entity\Rating;
+use Victoire\Widget\AggregateRatingBundle\Entity\WidgetAggregateRating;
 use Victoire\Widget\AggregateRatingBundle\Form\RatingType;
 
 /**
@@ -48,15 +54,56 @@ class RatingController extends Controller
             "businessEntityId" => $businessEntityId,
             "entityId" => $entityId
         ]);
+        $businessEntity = null;
+        try{
+            $businessEntity = $this->get('victoire_core.helper.business_entity_helper')->findById($businessEntityId);
+        }catch (\Exception $e){}
+
+        if($businessEntity)
+        {
+            $businessTemplates = $em->getRepository('VictoireBusinessPageBundle:BusinessTemplate')->findPagePatternByBusinessEntity($businessEntity);
+            foreach( $businessTemplates as $businessTemplate)
+            {
+                /** @var BusinessPage $page */
+                if($page = $em->getRepository(BusinessPage::class)->findPageByBusinessEntityAndPattern(
+                    $businessTemplate, $entityId, $businessEntity
+                ))
+                {
+                    $this->regenerateAssociatedWidgetsCache($page);
+                }
+            }
+
+        }elseif($page = $em->getRepository(Page::class)->findOneBy(['id'=> $entityId]))
+        {
+            $this->regenerateAssociatedWidgetsCache($page);
+        }
+
         return new JsonResponse(
             [
                 'success' => true,
-                "businessEntityId" => $businessEntityId,
-                "entityId" => $entityId,
+                'businessEntityId' => $businessEntityId,
+                'entityId' => $entityId,
                 'ratingRound' => $values['ratingRound'],
                 'ratingNumber' => $values['ratingNumber'],
             ]
         );
+    }
+    private function regenerateAssociatedWidgetsCache(View $view)
+    {
+        $em =$this->getDoctrine()->getManager();
+        $widgetCache = $this->get('victoire_widget.widget_cache');
+        /** @var Widget $widget */
+        $view->setReference($this->get('victoire_view_reference.repository')->findReferenceByView($view));
+        $widgets = $em->getRepository(WidgetAggregateRating::class)->findBy(['view' => $view]);
+        if(count($widgets) > 0)
+        {
+            foreach($widgets as $widget)
+            {
+                $widget->setCurrentView($view);
+                $widgetCache->remove($widget);
+            }
+        }
+
     }
 
 }
